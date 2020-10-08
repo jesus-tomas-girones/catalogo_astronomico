8011bc3b8d35e679ea3fcfa59f545d2127750d9b
Stella Heras <sheras@dsic.upv.es>
2020-10-08 10:33:15 +0200

    Update EstrellaTest.java

--- catalogo/src/test/java/com/example/catalogoastronomico/EstrellaTest.java
+++ catalogo/src/test/java/com/example/catalogoastronomico/EstrellaTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class EstrellaTest {
+/**
 
    @Test
    public void crear_objeto_y_getters() {
@@ -50,5 +51,6 @@ public class EstrellaTest {
       Estrella a = new Estrella("Alfa Centauri", -0.27, 4.5, 3, g);
       assertEquals("objeto-Astro {nombre: Alfa Centauri, tipo: Estrella, brillo: -0.27, distancia: 4.5, planetas: 3, galaxia: Vía Láctea}", a.toString());
    }
+**/
 
 }
